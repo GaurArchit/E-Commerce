c8eba0367cfb094aee9da7975e3a7944ea81fad6
Archit gaur <architgaur123@gmail.com>
2024-06-11 01:40:32 +0530

    This time sign up page has been updated

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 import express from "express";
 import list from "./database/data.js";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+//Handling static pages in E-commerce Project
+app.set("view engine", "pug");
+app.use("/public", express.static(join(__dirname, "public")));
 app.use(cors());
 dotenv.config();
 const mongoURI = process.env.monurl;
@@ -53,6 +60,12 @@ app.get("/price", async (req, res) => {
   res.json(data);
   console.log(data.length);
 });
+//Just and example this will be updated as per the logic
+app.post("/price", async (req, res) => {
+  const data = await Data.find({ price: { $lt: 100 } });
+  res.json(data);
+  console.log(data.length);
+});
 
 /// Get call in which products will be fetch based on the category name
 
@@ -60,6 +73,10 @@ app.get("/category/:zip", async (req, res) => {
   const categoryName = req.params.zip;
   const data = await Data.find({ category: categoryName });
   res.json(data);
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
 app.listen(7070, () => {
